1dfcc1cde7d0141738a9e445a071f4773509d998
lyj404 <lyj404@qq.com>
2025-08-29 21:00:35 +0800

    add comment

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,20 +5,21 @@ if status is-interactive
     set -g fish_greeting ""
 end
 
-# 设置代理
+# use proxy
 function proxy
     set -gx http_proxy "http://127.0.0.1:12334"
     set -gx https_proxy "http://127.0.0.1:12334"
     echo "✅ Proxy started"
 end
 
+# cancel proxy
 function unproxy
     set -e http_proxy
     set -e https_proxy
     echo "❌ Proxy stopped"
 end
 
-# 设置别名
+# setting alias
 alias sem="sudo -E emacs"
 alias sn="sudo -E nano"
 alias ls="exa --icons"
